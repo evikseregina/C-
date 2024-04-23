7a519112d852196a20db2879583648d3960fad7b
Eva Seregina <evaseregina04@gmail.com>
2024-04-23 13:53:00 +0300

    added 1st homework task

---
+++ homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿void Number (int num) {
+
+    if (num % 7 == 0 && num % 23 == 0) 
+    { 
+        Console.WriteLine($"число {num} кратно 7 и 23 одновременно");
+    }
+    else 
+    {
+         Console.WriteLine($"число {num} не кратно 7 и 23 одновременно");
+    }
+}
+ Number(161);
